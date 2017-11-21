8a7c88c0ad235a380668e5d33d30a896472f5384
Randy <carrion.randy@gmail.com>
2017-11-21 17:06:48 +0100

    routes updated

--- app.js
+++ app.js
@@ -7,47 +7,51 @@ var fs = require("fs")
 
 app.set('view engine', 'pug')
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     fs.readFile('./users.json', 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+        var read = JSON.parse(data)
+        res.render('index', { read: read })
+    })
+})
+app.post("/signup", (req,res)=> {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+        var read = JSON.parse(data)
+        read.push(req.body)
+        fs.writeFile("users.json", JSON.stringify(read), function(err) { //
             if (err) {
                 throw err;
             }
-            var read = JSON.parse(data)
-            res.render('index', { read: read })
+            console.log("reached")
+            res.redirect('/')
+        })
+    })
+})
+app.get("/signup", (req, res) => {
+    fs.readFile('./users.json', 'utf8', function(err, data) {
+        if (err) {
+            throw err;
+        }
+        var read = JSON.parse(data)
+        res.render('signup', { read: read })
     })
 })
 
-app.post("/", (req, res) => {
-    if (req.body.firstname !== "" && req.body.lastname !== "" && req.body.email !== "") {
-        fs.readFile('./users.json', 'utf8', function(err, data) {
-            if (err) {
-                throw err;
-            }
-            var read = JSON.parse(data)
-            read.push(req.body)
-            fs.writeFile("users.json", JSON.stringify(read), function(err) {
-                if (err) {
-                    throw err;
-                }
-                fs.readFile("users.json", "utf8", function(err) {
-                    if (err) {
-                        throw err;
-                    }
-                    var write1 = JSON.parse(data)
-                    console.log(JSON.stringify(write1) + "++")
-                })
-                res.render('index',{read:read})
-            })
-        })
-    }
+// function validateForm() {
+//     var x = document.forms["firstname"]["lastname"].value;
+//     if (x == "") {
+//         alert("Name must be filled out");
+//         return false;
+//     }
+// }
+app.get("/search", function(req, res){ ///page is thre page URL and 'page' is the route 
+    res.render('search')
 })
-function validateForm() {
-    var x = document.forms["firstname"]["lastname"].value;
-    if (x == "") {
-        alert("Name must be filled out");
-        return false;
-    }
-}console.log("oh babe")
 
 app.post("/search", function(req, res) {
     fs.readFile('./users.json', 'utf8', function(err, data) {
@@ -55,39 +59,33 @@ app.post("/search", function(req, res) {
             throw err;
         }
         var read = JSON.parse(data)
+        // console.log(req.body)
+        var match = []
         for (var i = 0; i < read.length; i++) {
-            if (req.body.firstname == read[i].firstname || req.body.lastname == read[i].lastname || req.body.email == read[i].email) {
-                res.render('search', {read:read})
+            if (req.body.search1 == read[i].firstname || req.body.search1 == read[i].lastname || req.body.search1 == read[i].email) {
+                // console.log(read[i])
+                match.push(read[i])
             } //this tells it to pull the string in body 'first'
-        }
+        }res.render('searchResults', {match:match})
     })
 })
 
+// app.post("/searchResults", function(req, res) {
+//     fs.readFile('./users.json', 'utf8', function(err, data) {
+//         if (err) {
+//             throw err;
+//         }
+//         res.render('searchResults')
+//     })
+// })x  
+// app.get("/contact", (req, res) => {
+//     res.render('Contact', {
+//         isLoggedIn: true,
+//         read.firstname, //-shopBasket is the key, List is the value
+//     })
+// })
 
 
 app.listen(3001, () => {
     console.log("Yes Honey?")
 })
-
-
-// fs.readFile('./users2.json', 'utf8', function(err, data){
-//     var oldList=JSON.parse(data)
-//     if (err) {
-//         throw err;
-//     } var newList= JSON.parse(data)
-//         var  
-//        var firstname=(req.body.first)
-//        var lastname=(req.body.last)
-//        var email=(req.body.email)
-
-// app.get("/", function(req, res) {
-//     res.render("index")
-// })
-
-// app.get("/contact", (req, res) => {
-//     let list = ["cake", "ducky", "Coke", "pills"] //-list is the var value
-//     res.render('Contact', {
-//         isLoggedIn: true,
-//         shopBasket: list, //-shopBasket is the key, List is the value
-//     })
-// })
